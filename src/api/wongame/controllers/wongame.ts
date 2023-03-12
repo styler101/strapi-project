b5132b9629d5f9d261a3bd5622f2758eedeb8c97
Lucas <lucas.santos@mundowap.com.br>
2023-03-12 12:35:57 -0300

    feat: add header

---
+++ src/api/wongame/controllers/wongame.ts
@@ -0,0 +1,7 @@
+/**
+ * wongame controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::wongame.wongame');
---
+++ src/api/wongame/routes/wongame.ts
@@ -0,0 +1,7 @@
+/**
+ * wongame router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::wongame.wongame');
